565b6f7083ad876673445013cb01b00c75b2f257
Bodo Kaiser <i@bodokaiser.io>
2017-01-16 13:02:29 +0100

    fixed image conversion to float in read_minc

--- ioutil/minc.py
+++ ioutil/minc.py
@@ -4,6 +4,5 @@ import numpy as np
 def read_minc(filename):
     with h5py.File(filename, 'r') as f:
         slices = np.array(f['minc-2.0/image/0/image'], np.float32)
-        slices -= np.min(slices)
-        slices /= np.max(slices)
-        return slices
+        vrange = np.array(f['minc-2.0/image/0/image'].attrs['valid_range'])
+        return (slices - vrange[0]) / np.sum(np.abs(vrange))
